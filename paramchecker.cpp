bace19d4257e71f3248b1f83c77ced767f3c56e5
Harsha M M <harsha.manjulamallikarjun@in.bosch.com>
2020-07-20 18:25:24 +0530

    Bringdown cyclomatic complexity to 3
    Signed-off-by: Harsha M M <harsha.manjulamallikarjun@in.bosch.com>

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,11 +1,38 @@
 
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
+bool bmpOk(float bpm)
+{
+  bool ret = true;
   if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 80) {
-    return false;
-  } else if(respRate < 30 || respRate > 60) {
-    return false;
+      ret = false;
   }
-  return true;
+  return ret;
+}
+
+bool spoOk(float spo2)
+{
+  bool ret = true;
+  if(spo2 < 80) {
+      ret = false;
+  }
+  return ret;
+}
+
+bool respRateOk(float respRate)
+{
+    bool ret = true;
+    if(respRate < 30 || respRate > 60)
+    {
+        ret = false;
+    }
+    return ret;
+}
+
+bool vitalsAreOk(float bpm, float spo2, float respRate)
+{
+    bool ret = bmpOk(bpm);
+    if (!ret)
+        ret = spoOk(spo2);
+    if (!ret)
+        ret = respRateOk(respRate);
+    return ret;
 }
